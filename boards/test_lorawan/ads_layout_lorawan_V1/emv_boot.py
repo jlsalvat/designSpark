9e1dde7c69aeda3af3363ebb2ee1f833e21a0021
jlsalvat <salvat@unice.fr>
2023-05-30 17:18:29 +0200

    update rc5 tiny

--- boards/test_lorawan/ads_layout_lorawan_V1/emv_boot.py
+++ boards/test_lorawan/ads_layout_lorawan_V1/emv_boot.py
@@ -8,9 +8,9 @@ def main():
 	subst=r"ads_layout_lorawan_V1_lib/tech.subst"
 	substlib=r"ads_layout_lorawan_V1_lib"
 	substname=r"tech"
-	cell=r"layout_lora"
+	cell=r"test_CMWX1ZZABZ-091"
 	view=r"layout"
-	libS3D=r"simulation/ads_layout_lorawan_V1_lib/layout_lora/_3%D%Viewer/extra/0/proj_libS3D.xml"
+	libS3D=r"simulation/ads_layout_lorawan_V1_lib/test_%C%M%W%X1%Z%Z%A%B%Z#2d091/_3%D%Viewer/extra/0/proj_libS3D.xml"
 	varDictionary={}
 	exprDictionary={}
 	adv.loadDesign(path=path, lib=lib, subst=subst, substlib=substlib, substname=substname, cell=cell, view=view, libS3D=libS3D, var_dict=varDictionary, expr_dict=exprDictionary)
